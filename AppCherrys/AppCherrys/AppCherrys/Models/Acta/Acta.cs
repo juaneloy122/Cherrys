01c53e8f1939969c3bdfe11f11601051b04d60a5
Toño <zaycordisco@hotmail.com>
2019-07-16 15:20:55 +0200

    Se crean pequeñas clases  para el modelo

---
+++ AppCherrys/AppCherrys/AppCherrys/Models/Acta/Acta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCherrys.Models.Acta
+{
+    /// <summary>
+    /// Acta publicada
+    /// </summary>
+    public class Acta
+    {
+        public string Id { get; set; }       
+        public string Description { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public string RutaDocumento { get; set; }
+
+        public List <string> RutaAdjuntos { get; set; }
+    }
+}
