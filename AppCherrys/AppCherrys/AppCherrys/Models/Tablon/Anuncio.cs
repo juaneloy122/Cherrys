012d0c3cc12d80dd2f90ca4ddf7e5d3b9e68fcf7
Toño <zaycordisco@hotmail.com>
2019-07-16 15:20:55 +0200

    Se crean pequeñas clases  para el modelo

---
+++ AppCherrys/AppCherrys/AppCherrys/Models/Tablon/Anuncio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCherrys.Models.Tablon
+{
+    /// <summary>
+    /// Anuncios publicados en el tablón de inicio
+    /// </summary>
+    public class Anuncio
+    {
+        public string Id { get; set; }        
+        public string Description { get; set; }
+
+        public DateTime FechaPublicacion { get; set; }
+        public string IdUsuarioPublicacion { get; set; }
+    }
+}
