e73042923bdee25ae58c166cd73d82df8b71563e
Toño <zaycordisco@hotmail.com>
2019-07-18 19:52:33 +0200

    Recolocación de la solución

---
+++ AppCherrys/AppCherrys/Models/Tablon/Anuncio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCherrys.Models.Tablon
+{
+    /// <summary>
+    /// Anuncios publicados en el tablón de inicio
+    /// </summary>
+    public class Anuncio
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+
+        public DateTime FechaPublicacion { get; set; }
+        public string IdUsuarioPublicacion { get; set; }
+    }
+}
