3735432b39891197a94a30ff8c7f4406401eba5f
Toño <zaycordisco@hotmail.com>
2019-07-24 15:23:10 +0200

    Se cambia a panel de navegación izquierdo

--- AppCherrys/AppCherrys/Views/MainPage.xaml.cs
+++ AppCherrys/AppCherrys/Views/MainPage.xaml.cs
@@ -8,11 +8,29 @@ namespace AppCherrys.Views
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : TabbedPage
+    public partial class MainPage : MasterDetailPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            masterPage.listView.ItemSelected += OnItemSelected;
+
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                MasterBehavior = MasterBehavior.Popover;
+            }
+        }
+
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as MasterPageItem;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                masterPage.listView.SelectedItem = null;
+                IsPresented = false;
+            }
         }
     }
 }
