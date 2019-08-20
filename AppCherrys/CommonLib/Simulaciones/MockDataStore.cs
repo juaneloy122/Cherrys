f0f4cbfeb70de1fc9d8958fae2a0f3e72a27cce2
Toño <zaycordisco@hotmail.com>
2019-08-20 14:53:50 +0200

    Se mueve la parte de almacén para simulaciones
    para la commonlib para poder aprovecharla también desde el servicio

---
+++ AppCherrys/CommonLib/Simulaciones/MockDataStore.cs
@@ -0,0 +1,51 @@
+﻿using CommonLib.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommonLib.Simulaciones
+{
+    public class MockDataStore <T>: IDataStore<T> where T: IItem
+    {
+        protected  List<T> Items;
+
+        public MockDataStore()
+        {
+           
+        }
+
+        public async Task<bool> AddItemAsync(T anuncio)
+        {
+            Items.Add(anuncio);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> UpdateItemAsync(T anuncio)
+        {
+            var oldItem = Items.Where((T arg) => arg.Id == anuncio.Id).FirstOrDefault();
+            Items.Remove(oldItem);
+            Items.Add(anuncio);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteItemAsync(int id)
+        {
+            var oldItem = Items.Where((T arg) => arg.Id == id).FirstOrDefault();
+            Items.Remove(oldItem);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<T> GetItemAsync(int id)
+        {
+            return await Task.FromResult(Items.FirstOrDefault(s => s.Id == id));
+        }
+
+        public async Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(Items);
+        }
+    }
+}
