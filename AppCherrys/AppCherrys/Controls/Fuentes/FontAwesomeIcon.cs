2151f075d3177e8cf282e205b41a8babafe290ed
Diaz <alfonsodiaz1996@gmail.com>
2019-08-31 14:47:02 +0200

    Cambiado login y master page. Ademas se añaden iconos

---
+++ AppCherrys/AppCherrys/Controls/Fuentes/FontAwesomeIcon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppCherrys.Controls.Fuentes
+{
+    public class FontAwesomeIcon : Label
+    {
+        public static string Typeface => Device.RuntimePlatform == Device.Android ? "fa-solid-900" : "FontAwesome5Free-Solid";
+
+        public FontAwesomeIcon()
+        {
+            FontFamily = Typeface;
+        }
+
+        public FontAwesomeIcon(string fontAwesomeIcon = null)
+        {
+            FontFamily = Typeface; //iOS is happy with this, Android needs a renderer to add ".ttf"
+            Text = fontAwesomeIcon;
+        }
+    }
+}
--- AppCherrys/AppCherrys/Views/MasterPageItem.cs
+++ AppCherrys/AppCherrys/Views/MasterPageItem.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using AppCherrys.Controls.Fuentes;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace AppCherrys.Views
 {
     public class MasterPageItem
     {
         public string Title { get; set; }
-        public string IconSource { get; set; }
+        public string Icon { get; set; }
         public Type TargetType { get; set; }
+        public Color ColorIcono { get; set; }
+        public double SizeIcono { get; set; }
     }
 }
