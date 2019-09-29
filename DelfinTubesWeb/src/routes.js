e59945cabdb4e44ccf76ce6750e2a859e9c83551
Diaz <alfonsodiaz1996@gmail.com>
2019-09-29 19:51:06 +0200

    Eliminados enlaces a conseguir premiun

--- DelfinTubesWeb/src/routes.js
+++ DelfinTubesWeb/src/routes.js
@@ -100,14 +100,6 @@ const dashboardRoutes = [
     icon: Language,
     component: RTLPage,
     layout: "/rtl"
-  },
-  {
-    path: "/upgrade-to-pro",
-    name: "Upgrade To PRO",
-    rtlName: "التطور للاحترافية",
-    icon: Unarchive,
-    component: UpgradeToPro,
-    layout: "/admin"
   }
 ];
 
